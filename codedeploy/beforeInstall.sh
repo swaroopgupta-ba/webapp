3f3c615ff69b6e7f9f475aedd3014481995c43ff
Swaroop Gupta <swaroopgupta.ba@gmail.com>
2021-11-04 15:55:05 -0400

    adding missing config file

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -2,7 +2,7 @@
 cd /home/ubuntu
 sudo rm -rf webapp
 echo "get the proces id"
-PID=`ps -eaf | grep "node server.js" | grep -v grep | awk '{print $2}'`
+PID=`ps -eaf | grep "node app.js" | grep -v grep | awk '{print $2}'`
 echo "process id not empty ? $PID"
 if [[ "" !=  "$PID" ]]; then
   echo "killing $PID"

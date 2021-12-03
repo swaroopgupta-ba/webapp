611d930b7fae20f159dd14dc87acaee653ac5ab8
Swaroop Gupta <swaroopgupta.ba@gmail.com>
2021-12-03 17:39:57 -0500

    fixing routes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const userRouter = require("./api/users/user.router");
 //convert user input to JSON
 app.use(express.json());
 
-app.use("/v1/user", userRouter);
+app.use("/v1", userRouter);
 
 app.listen(3000, () => {
   console.log("Server up and running at : ", 3000);
